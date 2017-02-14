c72277a1fa4409b4fb895a325edb7be6b3cfa224
Khalid Said <kerbrose@hotmail.com>
2017-02-14 09:45:15 +0200

    fix missing dependency, adding new fields

--- open_academy/models/library_book.py
+++ open_academy/models/library_book.py
@@ -7,10 +7,50 @@
 # @author: kerbrose
 #####################################
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 class LibraryBook(models.Model):
-    _name = 'library.book'
+    _name = 'library.book' # this is the unique id for the model
+    _description = 'Library Book' # this is a human readable name
+    _order = 'date_release desc, name' 
+    _rec_name = 'short_name'  # Alternative field to use as name, used by osv’s name_get() (default: 'name')
     name = fields.Char('Title', required=True)
-    date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
+    short_name = fields.Char(string='Short Title',
+                             size=100, # For Char only
+                             translate=False, # also for Text fields
+                             )
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'), ('available', 'Available'), ('lost', 'Lost')], 'State')
+    description = fields.Html(string='Description', # optional:
+                              sanitize=True,
+                              strip_style=False,
+                              translate=False,)
+    cover = fields.Binary('Book Cover')
+    date_release = fields.Date('Release Date')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer( string='Number of Pages',
+                            default=0,
+                            help='Total book pages count',
+                            groups='base.group_user',
+                            states={'cancel': [('readonly', True)]},
+                            copy=True,
+                            index=False,
+                            readonly=False,
+                            required=False,
+                            company_dependent=False,)
+    reader_rating = fields.Float( 'Reader Average Rating',
+                                  (14, 4), # Optional precision (total, decimals),
+                                  )
+
+
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            result.append(
+                (record.id, u"%s (%s)" % (record.name, record.date_released)
+                 ))
+        return result
